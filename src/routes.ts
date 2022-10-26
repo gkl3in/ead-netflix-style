c1763c7ef7b597c7e8a91dc909c29a6e929932d6
Gabriel Klein <gabriel.klein@philips.com>
2022-10-26 20:19:52 -0300

    Adicionando favoritos

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { authController } from './controllers/authController';
 import { categoriesController } from './controllers/categoriesController';
 import { coursesController } from './controllers/coursesController';
 import { episodesController } from './controllers/episodesController';
+import { favoritesController } from './controllers/favoritesController';
 import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth';
 
 const router = express.Router();
@@ -15,9 +16,11 @@ router.get('/categories/:id', ensureAuth, categoriesController.show);
 
 router.get('/courses/featured', ensureAuth, coursesController.featured);
 router.get('/courses/newest', coursesController.newest)
-router.get('/courses/search', ensureAuth, coursesController.search)
+router.get('/courses/search', ensureAuth, coursesController.search);
 router.get('/courses/:id', ensureAuth, coursesController.show);
 
 router.get('/episodes/stream', ensureAuthViaQuery, episodesController.stream);
+
+router.post('/favorites', ensureAuth, favoritesController.save);
 
 export { router };
