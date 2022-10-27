6d4ca529405f0a3fef7f6dc486a1fef151c9c782
Gabriel Klein <gabriel.klein@philips.com>
2022-10-27 19:57:25 -0300

    Excluindo favoritos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -29,4 +29,12 @@ export const favoriteService = {
 
     return favorite
   },
+  delete: async (userId: number, courseId: number) => {
+    await Favorite.destroy({
+      where: {
+        userId,
+        courseId
+      }
+    })
+  }
 };
