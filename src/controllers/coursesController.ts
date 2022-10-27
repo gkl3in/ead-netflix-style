ead64de0b1b12f9f81f2a6262facbb84c69f5297
Gabriel Klein <gabriel.klein@philips.com>
2022-10-27 20:38:05 -0300

    atualizando endpoint de cursos

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express'
 import { getPaginationParams } from '../helpers/getPaginationParams'
+import { AuthenticatedRequest } from '../middlewares/auth'
 import { courseService } from '../services/courseService'
+import { favoriteService } from '../services/favoriteService'
+import { likeService } from '../services/likeService'
 
 export const coursesController = {
   // GET /courses/featured
@@ -15,17 +18,17 @@ export const coursesController = {
     }
   },
 
-  	// GET /courses/newest
-    newest: async (req: Request, res: Response) => {
-      try {
-        const newestCourses = await courseService.getTopTenNewest()
-        return res.json(newestCourses)
-      } catch (err) {
-        if (err instanceof Error) {
-          return res.status(400).json({ message: err.message })
-        }
+  // GET /courses/newest
+  newest: async (req: Request, res: Response) => {
+    try {
+      const newestCourses = await courseService.getTopTenNewest()
+      return res.json(newestCourses)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
       }
-    },
+    }
+  },
 
     // GET /courses/search?name=
   search: async (req: Request, res: Response) => {
@@ -44,16 +47,23 @@ export const coursesController = {
   },
 
   // GET /courses/:id
-  show: async (req: Request, res: Response) => {
-    const { id } = req.params
+  show: async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user!.id
+    const courseId = req.params.id
 
     try {
-      const course = await courseService.findByIdWithEpisodes(id)
-      return res.json(course)
+        const course = await courseService.findByIdWithEpisodes(courseId);
+
+        if (!course) return res.status(404).json({ message: 'Curso não encontrado' });
+
+        const liked = await likeService.isLiked(userId, Number(courseId));
+        const favorited = await favoriteService.isFavorited(userId, Number(courseId));
+
+        return res.json({ ...course.get(), favorited, liked });
     } catch (err) {
-      if (err instanceof Error) {
-        return res.status(400).json({ message: err.message })
-      }
+        if (err instanceof Error) {
+            return res.status(400).json({ message: err.message });
+        }
     }
   }
 }
